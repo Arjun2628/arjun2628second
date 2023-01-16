15fd2b59cd37711417699864e00c177902f4122f
Arjun2628 <122284768+Arjun2628@users.noreply.github.com>
2023-01-16 17:47:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function btnblack(a ,b){
+    document.getElementById(""+a+"").style.backgroundColor='rgb(0, 0, 0)'
+    document.getElementById(""+b+"").style.color='rgb(255, 255, 255)'
+
+}
+function btnwhite(a,b){
+    document.getElementById(""+a+"").style.backgroundColor=null
+    document.getElementById(""+b+"").style.color='rgb(0, 0, 0)'
+
+}
+
+
+ 
